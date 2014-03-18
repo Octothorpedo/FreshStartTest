39aef449332180b2fc8d6e92a2bc6308d10aad43
Crystal08 <Crystal08@compuserve.com>
2014-03-18 13:43:22 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FreshStartTest::Application.config.secret_key_base = '5ddd5a94126c1e1c109e781bd7e9f045bb2bd7a1c5fc27a7c2867e60bd880d0341682ac5c0ca28af77a63b95e2531fc2c66c8d3ede58d01fb1d74c5ae9b5fd2e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FreshStartTest::Application.config.session_store :cookie_store, key: '_FreshStartTest_session'
